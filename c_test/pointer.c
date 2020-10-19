093166d7a3bca21831f4fce91a23bbb89a0ff7c2
thewind <yz15951606335@gmail.com>
2020-10-19 10:53:01 +0800

    add c language files

---
+++ c_test/pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// int x = 1;
+
+// int y = 2;
+// int z[10];
+
+// ip = &x;
+// y = *ip;
+// *ip = 0;
+// ip = &z[10];
+
+int main() {
+    char name[100];
+    printf("hello, world!\n");
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("Hello,%s,nice to meet you!\n",name);
+}
--- hello.py
+++ hello.py
@@ -7,3 +7,5 @@ import requests
 res = requests.get("https://www.baidu.com")
 
 print(res.status_code, res.text)
+
+list()
